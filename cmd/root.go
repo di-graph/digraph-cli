cfe82537d88cd9540b4af77ae0708da3390f9bf6
Bahar Shah <bahar@getdigraph.com>
2023-01-24 12:05:33 -0500

    bump

--- cmd/root.go
+++ cmd/root.go
@@ -11,7 +11,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const VERSION = "v0.0.27"
+const VERSION = "v0.0.28"
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
